531d7a8c53f21c5a0df071e41ea718ce47b62a5f
Rayne <pluvstream@gmail.com>
2025-05-26 17:56:03 +0900

    Update Chapter01

---
+++ Chapter01/src/SumVerbose2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SumVerbose2 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int inputNum;
+    System.out.println("1부터 n까지의 합 구하기");
+    do {
+      System.out.print("n의 값: ");
+      inputNum = scanner.nextInt();
+    } while (inputNum <= 0);
+
+    int sum = 0;
+    for (int i = 1; i < inputNum; i++) {
+      System.out.print(i + " + ");
+      sum += i;
+    }
+
+    System.out.print(inputNum + " = ");
+    sum += inputNum;
+    System.out.println(sum);
+  }
+}

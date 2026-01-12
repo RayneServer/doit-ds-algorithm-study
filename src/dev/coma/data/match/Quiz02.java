9873339dba8b0c78432c604507e43c6a79180dae
Rayne <pluvstream@gmail.com>
2026-01-12 23:59:02 +0900

    Create Chapter 07

---
+++ src/dev/coma/data/match/Quiz02.java
@@ -0,0 +1,68 @@
+package dev.coma.data.match;
+
+import java.util.Scanner;
+
+public class Quiz02 {
+  public static void main(String[] args) {
+    String input;
+    String word;
+
+    try (Scanner scanner = new Scanner(System.in)) {
+      System.out.print("텍스트: ");
+      input = scanner.nextLine();
+
+      System.out.print("패턴: ");
+      word = scanner.nextLine();
+    }
+
+    int resultIndex = bruteForce(input, word);
+
+    if (resultIndex == -1) System.out.println("텍스트에 패턴이 없습니다.");
+    else {
+      int length = 0;
+      for (int i = 0; i < resultIndex; i++) {
+        length += input.substring(i, i + 1).getBytes().length;
+      }
+
+      length += word.length();
+
+      System.out.println((resultIndex + 1) + "번째 문자부터 일치합니다.");
+      System.out.println("텍스트: " + input);
+      System.out.printf(String.format("패턴: %%%ds\n", length), word);
+    }
+  }
+
+  private static int bruteForce(String input, String word) {
+    int inputPointer = 0;
+    int wordPointer = 0;
+    int counter = 1;
+    boolean isMove = true;
+
+    while (inputPointer != input.length() && wordPointer != word.length()) {
+      if (isMove) {
+        System.out.println(counter++ + "회차");
+        isMove = false;
+      }
+
+      System.out.println(input);
+      System.out.print(" ".repeat(inputPointer));
+
+      if (input.charAt(inputPointer) == word.charAt(wordPointer)) {
+        System.out.println("+");
+        inputPointer++;
+        wordPointer++;
+      } else {
+        System.out.println("|");
+        inputPointer = inputPointer - wordPointer + 1;
+        wordPointer = 0;
+        isMove = true;
+      }
+
+      System.out.println(" ".repeat(counter - 2) + word);
+      System.out.println();
+    }
+
+    if (wordPointer == word.length()) return inputPointer - wordPointer;
+    return -1;
+  }
+}

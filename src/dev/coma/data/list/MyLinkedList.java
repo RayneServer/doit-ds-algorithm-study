28b37fa98cfc77c148f1c78db67d706238267be0
Rayne <pluvstream@gmail.com>
2026-01-17 18:46:25 +0900

    Create Chapter 08

---
+++ src/dev/coma/data/list/MyLinkedList.java
@@ -0,0 +1,172 @@
+package dev.coma.data.list;
+
+public class MyLinkedList<E extends Comparable<E>> {
+  private Node<E> head;
+  private Node<E> tail;
+  private int size;
+
+  private static class Node<E> {
+    private E data;
+    private Node<E> next;
+
+    public Node(E data) {
+      this.data = data;
+      this.next = null;
+    }
+
+    @Override
+    public String toString() {
+      return data.toString();
+    }
+  }
+
+  public MyLinkedList() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  public void addFirst(E e) {
+    Node<E> node = head;
+    head = new Node<>(e);
+    head.next = node;
+
+    if (tail == null) tail = head;
+
+    size++;
+  }
+
+  public void addLast(E e) {
+    if (head == null) {
+      addFirst(e);
+      return;
+    }
+
+    tail.next = new Node<>(e);
+    tail = tail.next;
+    size++;
+  }
+
+  public void removeFirst() {
+    if (head == null) throw new RuntimeException("head is null!");
+
+    head = head.next;
+    size--;
+  }
+
+  public void removeLast() {
+    if (head == null || head.next == null) {
+      removeFirst();
+      return;
+    }
+
+    Node<E> node = head;
+    while (node.next.next != null) {
+      node = node.next;
+    }
+
+    tail = node;
+    tail.next = null;
+    size--;
+  }
+
+  public void remove(E e) {
+    if (head == null) throw new RuntimeException("head is null!");
+
+    Node<E> node = head;
+    Node<E> prev = null;
+    while (node != null) {
+      if (node.data.compareTo(e) == 0) break;
+
+      prev = node;
+      node = node.next;
+    }
+
+    if (node == null) System.out.println("대상이 없습니다.");
+    else if (node == head) removeFirst();
+    else {
+      if (node == tail) tail = prev;
+
+      prev.next = node.next;
+      size--;
+    }
+  }
+
+  public E search(E e) {
+    Node<E> node = head;
+
+    while (node != null) {
+      if (node.data.compareTo(e) == 0) return node.data;
+
+      node = node.next;
+    }
+
+    return null;
+  }
+
+  public void clear() {
+    head = null;
+    tail = null;
+    size = 0;
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public void purge() {
+    Node<E> node = head;
+
+    while (node.next != null) {
+      Node<E> selectNode = node.next;
+      Node<E> prevNode = node;
+
+      while (selectNode != null) {
+        if (selectNode.data.compareTo(node.data) == 0) {
+          prevNode.next = selectNode.next;
+
+          selectNode = selectNode.next;
+        } else {
+          prevNode = selectNode;
+          selectNode = selectNode.next;
+        }
+      }
+
+      node = node.next;
+    }
+  }
+
+  public E retrieve(int count) {
+    if (count > size) return null;
+
+    int counter = 1;
+    Node<E> node = head;
+
+    while (counter < count) {
+      node = node.next;
+      counter++;
+    }
+
+    return node.data;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.append("[");
+
+    if (head != null) {
+      Node<E> node = head;
+
+      while (node != null) {
+        stringBuilder.append(node + ", ");
+        node = node.next;
+      }
+
+      stringBuilder.replace(stringBuilder.length() - 2, stringBuilder.length(), "");
+    }
+
+    stringBuilder.append("]");
+    return stringBuilder.toString();
+  }
+}
---
+++ src/dev/coma/data/list/MyLinkedListMain.java
@@ -0,0 +1,130 @@
+package dev.coma.data.list;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class MyLinkedListMain {
+  private static final Scanner SC = new Scanner(System.in);
+  private static final MyLinkedList<Data> DATA_LIST = new MyLinkedList<>();
+
+  public static void main(String[] args) {
+    while (true) {
+      Menu menu = selectMenu();
+
+      if (menu == Menu.TERMINATE) {
+        System.out.println("프로그램을 종료합니다.");
+        return;
+      }
+
+      if (menu == null) {
+        System.out.println("올바른 번호를 입력해주세요.\n");
+        continue;
+      }
+
+      activeMenu(menu);
+    }
+  }
+
+  private static Menu selectMenu() {
+    for (Menu menu : Menu.values()) {
+      if (menu.ordinal() != 0 && menu.ordinal() % 3 == 0) System.out.println();
+
+      System.out.printf("(%d) %s ", menu.ordinal(), menu.getMessage());
+    }
+
+    System.out.print("\n메뉴를 선택하세요: ");
+    int input = SC.nextInt();
+
+    for (Menu menu : Menu.values()) {
+      if (menu.ordinal() == input) return menu;
+    }
+
+    return null;
+  }
+
+  private static void activeMenu(Menu menu) {
+    switch (menu) {
+      case ADD_FIRST -> addFirst();
+      case ADD_LAST -> addLast();
+      case RMV_FIRST -> removeFirst();
+      case RMV_LAST -> removeLast();
+      case RMV_DATA -> remove();
+      case SEARCH -> search();
+      case CLEAR -> clear();
+      case SIZE -> size();
+      case PURGE -> purge();
+      case RETRIEVE -> retrieve();
+      case PRINT_ALL -> printAll();
+    }
+  }
+
+  private static void addFirst() {
+    System.out.println("머리에 추가할 데이터를 입력하세요.");
+    DATA_LIST.addFirst(createData());
+  }
+
+  private static void addLast() {
+    System.out.println("꼬리에 추가할 데이터를 입력하세요.");
+    DATA_LIST.addLast(createData());
+  }
+
+  private static void removeFirst() {
+    DATA_LIST.removeFirst();
+  }
+
+  private static void removeLast() {
+    DATA_LIST.removeLast();
+  }
+
+  private static void remove() {
+    System.out.println("삭제할 번호를 입력하세요.");
+    System.out.print("번호: ");
+    int num = SC.nextInt();
+
+    DATA_LIST.remove(new Data(num, null));
+  }
+
+  private static void search() {
+    System.out.println("검색할 번호를 입력하세요.");
+    System.out.print("번호: ");
+    int num = SC.nextInt();
+
+    Data data = DATA_LIST.search(new Data(num, null));
+    System.out.println(Objects.isNull(data) ? "데이터가 없습니다." : "검색 성공: " + data);
+  }
+
+  private static void clear() {
+    DATA_LIST.clear();
+  }
+
+  private static void size() {
+    System.out.println("현재 크기: " + DATA_LIST.size());
+  }
+
+  private static void purge() {
+    DATA_LIST.purge();
+  }
+
+  private static void retrieve() {
+    System.out.print("숫자를 입력해주세요: ");
+    int count = SC.nextInt();
+
+    Data result = DATA_LIST.retrieve(count);
+    System.out.println(Objects.isNull(result) ? "입력한 숫자가 리스트 크기보다 큽니다." : count + "번째 데이터 = " + result);
+  }
+
+  private static void printAll() {
+    System.out.println(DATA_LIST);
+  }
+
+  private static Data createData() {
+    System.out.print("번호: ");
+    int num = SC.nextInt();
+    SC.nextLine();
+
+    System.out.print("이름: ");
+    String name = SC.nextLine();
+
+    return new Data(num, name);
+  }
+}
